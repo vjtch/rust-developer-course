9a5c9094c4bff665c3d555a25b6c895d198213a3
Vojtěch Mikulenka <vjtch@proton.me>
2023-10-07 18:21:53 +0200

    hw_02

---
+++ hw_02_syntax_and_variables/src/main.rs
@@ -0,0 +1,67 @@
+use sha256::digest;
+use slug::slugify;
+use std::{env, io};
+use unicode_segmentation::UnicodeSegmentation;
+
+enum Transformation {
+    Lowercase,
+    Uppercase,
+    NoSpaces,
+    Slugify,
+    Reverse,
+    Sha256,
+}
+
+impl Transformation {
+    fn from(string: &str) -> Result<Transformation, String> {
+        match string {
+            "lowercase" => Ok(Transformation::Lowercase),
+            "uppercase" => Ok(Transformation::Uppercase),
+            "no-spaces" => Ok(Transformation::NoSpaces),
+            "slugify" => Ok(Transformation::Slugify),
+            "reverse" => Ok(Transformation::Reverse),
+            "sha256" => Ok(Transformation::Sha256),
+            _ => Err(String::from(
+                "lowercase | uppercase | no-spaces | slugify | reverse | sha256",
+            )),
+        }
+    }
+
+    fn action(&self, string: &str) -> String {
+        match self {
+            Transformation::Lowercase => string.to_lowercase(),
+            Transformation::Uppercase => string.to_uppercase(),
+            Transformation::NoSpaces => string.replace(" ", ""),
+            Transformation::Slugify => slugify(string),
+            Transformation::Reverse => string.graphemes(true).rev().collect(),
+            Transformation::Sha256 => String::from(digest(string)),
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("There should be exactly one CLI argument!");
+        return;
+    }
+
+    let transformation =
+        Transformation::from(&args[1].as_str()).expect("Invalid transformation name");
+
+    let mut input = String::new();
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => println!(
+            "{}",
+            transformation.action(
+                &input
+                    .trim_end_matches("\r\n")
+                    .trim_end_matches('\n')
+                    .trim_end_matches('\r')
+            )
+        ),
+        Err(error) => println!("Could not read input. (error: {error})"),
+    }
+}
